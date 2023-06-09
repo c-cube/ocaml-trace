b6322d060425b3abfbf215901b1cb37bf7c49110
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-06-09 10:23:43 -0400

    fix test: do not use feature not present in 4.08

--- test/t1.ml
+++ test/t1.ml
@@ -1,6 +1,6 @@
 let run () =
   for _i = 1 to 50 do
-    Trace.with_span ~__FUNCTION__ ~__FILE__ ~__LINE__ "outer.loop" @@ fun _sp ->
+    Trace.with_span ~__FILE__ ~__LINE__ "outer.loop" @@ fun _sp ->
     for _j = 2 to 5 do
       Trace.with_span ~__FILE__ ~__LINE__ "inner.loop" @@ fun _sp ->
       Trace.messagef ~__FILE__ ~__LINE__ (fun k -> k "hello %d %d" _i _j)

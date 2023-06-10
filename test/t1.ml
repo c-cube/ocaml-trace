3db8f73dcb19b64b4dbf286661811d83dbfb50ab
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-06-09 23:06:03 -0400

    update test with counters

--- test/t1.ml
+++ test/t1.ml
@@ -1,11 +1,16 @@
 let run () =
   Trace.set_process_name "main";
   Trace.set_thread_name "t1";
+
+  let n = ref 0 in
+
   for _i = 1 to 50 do
     Trace.with_span ~__FILE__ ~__LINE__ "outer.loop" @@ fun _sp ->
     for _j = 2 to 5 do
+      incr n;
       Trace.with_span ~__FILE__ ~__LINE__ "inner.loop" @@ fun _sp ->
-      Trace.messagef (fun k -> k "hello %d %d" _i _j)
+      Trace.messagef (fun k -> k "hello %d %d" _i _j);
+      Trace.counter_int "n" !n
     done
   done
 

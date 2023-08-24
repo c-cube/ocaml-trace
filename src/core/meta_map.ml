43c20fa2dc549b33746473215bc7641ea3a0bce3
Corentin Leruth <corentin.leruth@gmail.com>
2023-08-24 07:52:25 +0200

    inline module Type instead of generating

--- src/core/meta_map.ml
+++ src/core/meta_map.ml
@@ -1,4 +1,33 @@
-module Type = Type_
+(* Type identifiers, can be deleted once OCaml >= 5.1 is required *)
+module Type = struct
+  type (_, _) eq = Equal : ('a, 'a) eq
+
+  module Id = struct
+    type _ id = ..
+
+    module type ID = sig
+      type t
+      type _ id += Id : t id
+    end
+
+    type 'a t = (module ID with type t = 'a)
+
+    let make (type a) () : a t =
+      (module struct
+        type t = a
+        type _ id += Id : t id
+      end)
+
+    let provably_equal (type a b) ((module A) : a t) ((module B) : b t) :
+        (a, b) eq option =
+      match A.Id with
+      | B.Id -> Some Equal
+      | _ -> None
+
+    let uid (type a) ((module A) : a t) =
+      Obj.Extension_constructor.id (Obj.Extension_constructor.of_val A.Id)
+  end
+end
 
 module Key = struct
   type 'a t = 'a Type.Id.t

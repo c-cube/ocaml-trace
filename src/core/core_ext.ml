5018a9ead7db298f3003eef41943de563b022a98
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-01-14 20:29:33 -0500

    refactor core library
    much cleaner now, and simpler.
    
    - `span` is an open sum type, so it can carry as much info as we want
    from enter to exit. This means most collectors shouldn't even need a
    global table for spans!
    - merge manual spans and spans. It's just spans now. Keep old
    Trace_core.xxx_manual_xxx functions but deprecate them
    - add `extension_parameter` to carry additional info to collector (eg
    OTEL span kind, TEF sync/async, maybe span links, etc)
    - make the collector into a record of callbacks + a state, for
    efficiency (but also to make it easier to extend in the future with
    callbacks that have a default implementation
    )

---
+++ src/core/core_ext.ml
@@ -0,0 +1,15 @@
+(** A few core extensions.
+
+    @since NEXT_RELEASE *)
+
+open Types
+
+(** Additional extensions *)
+type extension_event +=
+  | Extension_set_thread_name of string
+  | Extension_set_process_name of string
+
+(** Specialized parameters *)
+type extension_parameter +=
+  | Extension_span_flavor of [ `Sync | `Async ]
+        (** Tell the backend if this is a sync or async span *)
--- src/core/trace_core.ml
+++ src/core/trace_core.ml
@@ -3,13 +3,14 @@ module A = Atomic_
 module Collector = Collector
 module Meta_map = Meta_map
 module Level = Level
+module Core_ext = Core_ext
 
-type collector = (module Collector.S)
+type collector = Collector.t
 
 (* ## globals ## *)
 
 (** Global collector. *)
-let collector : collector option A.t = A.make None
+let collector : collector A.t = A.make Collector.C_none
 
 (* default level for spans without a level *)
 let default_level_ = A.make Level.Trace
@@ -17,16 +18,8 @@ let current_level_ = A.make Level.Trace
 
 (* ## implementation ## *)
 
-let[@inline] ctx_of_span (sp : explicit_span) : explicit_span_ctx =
-  { span = sp.span; trace_id = sp.trace_id }
-
 let data_empty_build_ () = []
-
-let[@inline] enabled () =
-  match A.get collector with
-  | None -> false
-  | Some _ -> true
-
+let[@inline] enabled () = Collector.is_some (A.get collector)
 let[@inline] get_default_level () = A.get default_level_
 let[@inline] set_default_level l = A.set default_level_ l
 let[@inline] set_current_level l = A.set current_level_ l
@@ -35,152 +28,154 @@ let[@inline] get_current_level () = A.get current_level_
 let[@inline] check_level ?(level = A.get default_level_) () : bool =
   Level.leq level (A.get current_level_)
 
-let with_span_collector_ (module C : Collector.S) ?__FUNCTION__ ~__FILE__
-    ~__LINE__ ?(data = data_empty_build_) name f =
-  let data = data () in
-  C.with_span ~__FUNCTION__ ~__FILE__ ~__LINE__ ~data name f
+let parent_of_span_opt_opt = function
+  | None -> P_unknown
+  | Some None -> P_none
+  | Some (Some p) -> P_some p
 
-let[@inline] with_span ?level ?__FUNCTION__ ~__FILE__ ~__LINE__ ?data name f =
+let enter_span_st st (cbs : _ Collector.Callbacks.t) ?__FUNCTION__ ~__FILE__
+    ~__LINE__ ?parent ?(params = []) ?(data = data_empty_build_) name : span =
+  let parent = parent_of_span_opt_opt parent in
+  let data = data () in
+  cbs.enter_span st ~__FUNCTION__ ~__FILE__ ~__LINE__ ~parent ~params ~data name
+
+let with_span_collector_ st (cbs : _ Collector.Callbacks.t) ?__FUNCTION__
+    ~__FILE__ ~__LINE__ ?parent ?params ?data name f =
+  let sp : span =
+    enter_span_st st cbs ?__FUNCTION__ ~__FILE__ ~__LINE__ ?parent ?params ?data
+      name
+  in
+  match f sp with
+  | res ->
+    cbs.exit_span st sp;
+    res
+  | exception exn ->
+    let bt = Printexc.get_raw_backtrace () in
+    cbs.exit_span st sp;
+    Printexc.raise_with_backtrace exn bt
+
+let[@inline] with_span ?level ?__FUNCTION__ ~__FILE__ ~__LINE__ ?parent ?params
+    ?data name f =
   match A.get collector with
-  | Some collector when check_level ?level () ->
-    with_span_collector_ collector ?__FUNCTION__ ~__FILE__ ~__LINE__ ?data name
-      f
+  | C_some (st, cbs) when check_level ?level () ->
+    with_span_collector_ st cbs ?__FUNCTION__ ~__FILE__ ~__LINE__ ?parent
+      ?params ?data name f
   | _ ->
     (* fast path: no collector, no span *)
     f Collector.dummy_span
 
-let[@inline] enter_span ?level ?__FUNCTION__ ~__FILE__ ~__LINE__
-    ?(data = data_empty_build_) name : span =
+let[@inline] enter_span ?level ?__FUNCTION__ ~__FILE__ ~__LINE__ ?parent ?params
+    ?data name : span =
   match A.get collector with
-  | Some (module C) when check_level ?level () ->
-    let data = data () in
-    C.enter_span ~__FUNCTION__ ~__FILE__ ~__LINE__ ~data name
+  | C_some (st, cbs) when check_level ?level () ->
+    (enter_span_st [@inlined never]) st cbs ?__FUNCTION__ ~__FILE__ ~__LINE__
+      ?parent ?params ?data name
   | _ -> Collector.dummy_span
 
 let[@inline] exit_span sp : unit =
   match A.get collector with
-  | None -> ()
-  | Some (module C) -> C.exit_span sp
-
-let enter_manual_span_collector_ (module C : Collector.S) ~parent ~flavor
-    ?__FUNCTION__ ~__FILE__ ~__LINE__ ?(data = data_empty_build_) name :
-    explicit_span =
-  let data = data () in
-  C.enter_manual_span ~parent ~flavor ~__FUNCTION__ ~__FILE__ ~__LINE__ ~data
-    name
-
-let[@inline] enter_manual_span ~parent ?flavor ?level ?__FUNCTION__ ~__FILE__
-    ~__LINE__ ?data name : explicit_span =
-  match A.get collector with
-  | Some coll when check_level ?level () ->
-    enter_manual_span_collector_ coll ~parent ~flavor ?__FUNCTION__ ~__FILE__
-      ~__LINE__ ?data name
-  | _ -> Collector.dummy_explicit_span
-
-let[@inline] enter_manual_toplevel_span ?flavor ?level ?__FUNCTION__ ~__FILE__
-    ~__LINE__ ?data name : explicit_span =
-  enter_manual_span ~parent:None ?flavor ?level ?__FUNCTION__ ~__FILE__
-    ~__LINE__ ?data name
-
-let[@inline] enter_manual_sub_span ~parent ?flavor ?level ?__FUNCTION__
-    ~__FILE__ ~__LINE__ ?data name : explicit_span =
-  enter_manual_span
-    ~parent:(Some (ctx_of_span parent))
-    ?flavor ?level ?__FUNCTION__ ~__FILE__ ~__LINE__ ?data name
-
-let[@inline] exit_manual_span espan : unit =
-  if espan != Collector.dummy_explicit_span then (
-    match A.get collector with
-    | None -> ()
-    | Some (module C) -> C.exit_manual_span espan
-  )
+  | C_none -> ()
+  | C_some (st, cbs) -> cbs.exit_span st sp
 
 let[@inline] add_data_to_span sp data : unit =
   if sp != Collector.dummy_span && data <> [] then (
     match A.get collector with
-    | None -> ()
-    | Some (module C) -> C.add_data_to_span sp data
+    | C_none -> ()
+    | C_some (st, cbs) -> cbs.add_data_to_span st sp data
   )
 
-let[@inline] add_data_to_manual_span esp data : unit =
-  if esp != Collector.dummy_explicit_span && data <> [] then (
-    match A.get collector with
-    | None -> ()
-    | Some (module C) -> C.add_data_to_manual_span esp data
-  )
-
-let message_collector_ (module C : Collector.S) ?span
+let message_collector_ st (cbs : _ Collector.Callbacks.t) ?span ?(params = [])
     ?(data = data_empty_build_) msg : unit =
   let data = data () in
-  C.message ?span ~data msg
+  cbs.message st ~span ~params ~data msg
 
-let[@inline] message ?level ?span ?data msg : unit =
+let[@inline] message ?level ?span ?params ?data msg : unit =
   match A.get collector with
-  | Some coll when check_level ?level () ->
-    message_collector_ coll ?span ?data msg
+  | C_some (st, cbs) when check_level ?level () ->
+    (message_collector_ [@inlined never]) st cbs ?span ?params ?data msg
   | _ -> ()
 
-let messagef ?level ?span ?data k =
+let messagef ?level ?span ?params ?data k =
   match A.get collector with
-  | Some (module C) when check_level ?level () ->
+  | C_some (st, cbs) when check_level ?level () ->
     k (fun fmt ->
         Format.kasprintf
-          (fun str ->
-            let data =
-              match data with
-              | None -> []
-              | Some f -> f ()
-            in
-            C.message ?span ~data str)
+          (fun str -> message_collector_ st cbs ?span ?params ?data str)
           fmt)
   | _ -> ()
 
-let counter_int ?level ?(data = data_empty_build_) name n : unit =
+let counter_int ?level ?(params = []) ?(data = data_empty_build_) name n : unit
+    =
   match A.get collector with
-  | Some (module C) when check_level ?level () ->
+  | C_some (st, cbs) when check_level ?level () ->
     let data = data () in
-    C.counter_int ~data name n
+    cbs.counter_int st ~params ~data name n
   | _ -> ()
 
-let counter_float ?level ?(data = data_empty_build_) name f : unit =
+let counter_float ?level ?(params = []) ?(data = data_empty_build_) name f :
+    unit =
   match A.get collector with
-  | Some (module C) when check_level ?level () ->
+  | C_some (st, cbs) when check_level ?level () ->
     let data = data () in
-    C.counter_float ~data name f
+    cbs.counter_float st ~params ~data name f
   | _ -> ()
-
-let set_thread_name name : unit =
-  match A.get collector with
-  | None -> ()
-  | Some (module C) -> C.name_thread name
-
-let set_process_name name : unit =
-  match A.get collector with
-  | None -> ()
-  | Some (module C) -> C.name_process name
 
 let setup_collector c : unit =
   while
     let cur = A.get collector in
     match cur with
-    | Some _ -> invalid_arg "trace: collector already present"
-    | None -> not (A.compare_and_set collector cur (Some c))
+    | C_some _ -> invalid_arg "trace: collector already present"
+    | C_none -> not (A.compare_and_set collector cur c)
   do
     ()
   done
 
 let shutdown () =
-  match A.exchange collector None with
-  | None -> ()
-  | Some (module C) -> C.shutdown ()
+  match A.exchange collector C_none with
+  | C_none -> ()
+  | C_some (st, cbs) -> cbs.shutdown st
 
 type extension_event = Types.extension_event = ..
 
-let[@inline] extension_event ev =
+let[@inline] extension_event ev : unit =
   match A.get collector with
-  | None -> ()
-  | Some (module C) -> C.extension_event ev
+  | C_none -> ()
+  | C_some (st, cbs) -> cbs.extension st ev
+
+let set_thread_name name : unit =
+  extension_event @@ Core_ext.Extension_set_thread_name name
+
+let set_process_name name : unit =
+  extension_event @@ Core_ext.Extension_set_process_name name
 
 module Internal_ = struct
   module Atomic_ = Atomic_
 end
+
+(* ### deprecated *)
+
+[@@@ocaml.alert "-deprecated"]
+
+let enter_manual_span ~parent ?flavor ?level ?__FUNCTION__ ~__FILE__ ~__LINE__
+    ?data name : explicit_span =
+  let params =
+    match flavor with
+    | None -> []
+    | Some f -> [ Core_ext.Extension_span_flavor f ]
+  in
+  enter_span ~parent ~params ?level ?__FUNCTION__ ~__FILE__ ~__LINE__ ?data name
+
+let enter_manual_toplevel_span ?flavor ?level ?__FUNCTION__ ~__FILE__ ~__LINE__
+    ?data name : explicit_span =
+  enter_manual_span ~parent:None ?flavor ?level ?__FUNCTION__ ~__FILE__
+    ~__LINE__ ?data name
+
+let enter_manual_sub_span ~parent ?flavor ?level ?__FUNCTION__ ~__FILE__
+    ~__LINE__ ?data name : explicit_span =
+  enter_manual_span ~parent:(Some parent) ?flavor ?level ?__FUNCTION__ ~__FILE__
+    ~__LINE__ ?data name
+
+let exit_manual_span = exit_span
+let add_data_to_manual_span = add_data_to_span
+
+[@@@ocaml.alert "+deprecated"]
